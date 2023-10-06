10a438462dc5baf67e0307125bf2232d35a411be
Erick <114015229+khantako3132@users.noreply.github.com>
2023-10-06 15:13:23 +0630

    Javascript Chagnes

---
+++ offshore-master/about/index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<title>About Us│COOSY 株式会社クーシー｜Webデザイン/Web制作会社</title>
+<meta name="description" content="クーシーのビジョンについて、サービス、ものづくりへの考え方、働き方への取り組み方をご紹介します。" />
+<?php include("../include/head_favicon.php"); ?>
+<?php include("../include/head_css.php"); ?>
+<!-- ページ個別CSS -->
+<link rel="stylesheet" href="/assets/css/about/index.css">
+<!-- /ページ個別CSS -->
+<?php include("../include/head_js.php"); ?>
+</head>
+<body>
+  <?php include("../include/header.php"); ?>
+  <main class="main">
+    <div class="main__inner">
+      <div class="main__keyVisual">
+        <h1 class="main__keyVisual__title"><span class="title__shoulder">クーシーについて</span><span class="title__large">About Us</span></h1>
+        <img class="main__keyVisual__image" src="/assets/img/about/about_kv.png" alt="クーシーについて">
+        <p class="main__keyVisual__lead">インターネットのあらゆる課題を<br>「デザイン思考」で解決する</p>
+      </div>
+      <p class="main__lead">クーシーは1999年11月に設立、数多くの大規模企業Webサイトの制作・運用を中心に活動してきました。各案件の窓口であり中枢として統括業務を行う「東京本社」、Webサービス・アプリの制作・開発の実務を担当する「クーシーラボ 岩手」、東京郊外のガレージでプロダクト制作を行うモノヅクリの発信基地「クーシーモータース」の3ヶ所の拠点で事業展開をしています。Web制作のあらゆる工程に精通し、それを全て内政で行えるオールインワンの制作体制、経験に基づく的確なWebコンサルティング、アプリ開発など、Webサービスに関する幅広い業務を行うクリエイティブエージェンシーです。</p>
+      <p class="main__toProfile"><a href="/about/profile/">企業情報を見る</a></p>
+      <section class="main__section">
+        <div class="main__section__title">
+          <p class="sub">クーシーのビジョン</p>
+          <h2 class="eng">Our Vision</h2>
+        </div>
+        <div class="main__section__visionContents">
+          <section class="section section--service">
+            <div class="section__title">
+              <p class="section__title__sub">サービスについて</p>
+              <h3 class="section__title__text">デザイン思考のビジネスモデル<br class="styleSp">「設計思考」と「考える」こと。</h3>
+            </div>
+            <p class="section__text">最近「デザイン思考」という言葉がありますが、我々はデザインを「設計思考」と「考える力」と捉えています。そして「デザイン思考」は、ユーザーが使う時に分かりやすく、使いやすい、そして他とは違う体験を提供することを最優先とします。デザイン会社からスタートして、大規模なWeb制作のソリューションを提案できるようになったのは、システム開発やWebマーケティングにも、このデザイン思考を取り入れているからです。<br>現在はWebマーケティング、デザイン、システム開発といった領域で、技術と知識を使ってお客様が持つインターネットの課題を解決しています。ビジネスとしてはお客様の資産を作って対価を得ています。今後は「デザイン思考」と今まで培ってきた経験で我々の資産を作っていくことを考えています。</p>
+            <p class="section__link"><a href="/service/">サービスについて</a></p>
+          </section>
+          <section class="section section--creative">
+            <div class="section__title">
+              <p class="section__title__sub">ものづくりへの考え方</p>
+              <h3 class="section__title__text">好奇心の探求ができる場所。<br class="styleSp">そして、未来のはたらき方を考える。</h3>
+            </div>
+            <p class="section__text">クーシーモータースという別会社を東京郊外のあきる野市に設立しています。車を中心に、バイク、自転車、スケボーなど、タイヤが付いている乗り物をこよなく愛する人のためのプロダクトを作るため、元工場だった建物をガレージへと改造。まだまだ週末ごとにペンキ塗りなどを行なっている段階ですが、車のエンジンを電気にカスタムするなど、スタートさせています。このクーシーモータスも我々の資産を作っていく一つの手段です。「デザイン思考」は色々なアイデアを考え、様々な組み合わせを試行錯誤して最適なものを見つけ出すこと。資産作りもはたらく環境も色々なアイデアと様々な組み合わせで最適なものを創っていきたいと思います。</p>
+            <p class="section__link"><a href="/about/motors/">モータースについて</a></p>
+          </section>
+          <section class="section">
+            <div class="section__title">
+              <p class="section__title__sub">働き方への取り組み方</p>
+              <h3 class="section__title__text">未来のはたらき方を考える<br class="styleSp">「都合の良いカンケー」。</h3>
+            </div>
+            <p class="section__text">クーシーは、はたらき方は「都合の良いカンケー」がコンセプト。このコンセプトも「デザイン思考」から発想しています。はたらく人のインターフェイスとして会社があり、その会社がはたらきやすいか、便利なものか、そして他の会社では得られない経験ができるか。一人一人がマジメに仕事に取り組み成果と向き合えば、会社は自然と効率化していき利益は上がります。その利益をはたらきやすい会社に還元することによって、はたらく人の恩恵を広げることにつながります。会社もはたらく人もお互い実践することで「信頼関係」は築かれて、やがて「都合の良いカンケー」を築けます。</p>
+            <p class="section__link"><a href="/recruit/">リクルートについて</a></p>
+          </section>
+        </div>
+      </section>
+      <section class="main__section">
+        <div class="main__section__title">
+          <p class="sub">ニュース</p>
+          <h2 class="eng">News</h2>
+        </div>
+        <ul class="main__section__newsList">
+          <li class="item">
+            <a href="#">
+              <section class="item__inner">
+                <h2 class="item__title">制作実績に「XXXXXXXX」を追加しました。</h2>
+                <p class="item__date"><time>2019.01.01</time></p>
+              </section>
+            </a>
+          </li>
+          <li class="item">
+            <a href="#">
+              <section class="item__inner">
+                <h2 class="item__title">制作実績に「XXXXXXXX」を追加しました。</h2>
+                <p class="item__date"><time>2019.01.01</time></p>
+              </section>
+            </a>
+          </li>
+          <li class="item">
+            <a href="#">
+              <section class="item__inner">
+                <h2 class="item__title">制作実績に「XXXXXXXX」を追加しました。</h2>
+                <p class="item__date"><time>2019.01.01</time></p>
+              </section>
+            </a>
+          </li>
+          <li class="item">
+            <a href="#">
+              <section class="item__inner">
+                <h2 class="item__title">制作実績に「XXXXXXXX」を追加しました。</h2>
+                <p class="item__date"><time>2019.01.01</time></p>
+              </section>
+            </a>
+          </li>
+          <li class="item">
+            <a href="#">
+              <section class="item__inner">
+                <h2 class="item__title">制作実績に「XXXXXXXX」を追加しました。</h2>
+                <p class="item__date"><time>2019.01.01</time></p>
+              </section>
+            </a>
+          </li>
+          <li class="item">
+            <a href="#">
+              <section class="item__inner">
+                <h2 class="item__title">制作実績に「XXXXXXXX」を追加しました。</h2>
+                <p class="item__date"><time>2019.01.01</time></p>
+              </section>
+            </a>
+          </li>
+        </ul>
+        <p class="main__section__link"><a href="/news/" class="btn_square-inner">ニュース一覧を見る</a></p>
+      </section>
+      <div class="main__section">
+        <ul class="main__section__familyList">
+          <li class="item">
+            <a href="/about/tokyo/">
+              <section class="item__inner">
+                <figure class="item__img"><img src="/assets/img/about/about_img01.jpg" alt="株式会社クーシー 東京本社"></figure>
+                <h2 class="item__title"><span>Tokyo Office</span><small>株式会社クーシー 東京本社</small></h2>
+                <p class="item__text">東京の恵比寿にあるクーシー本社です。<br>プロデューサー、アートディレクターなど<br class="styleSp">40名が在籍しています。</p>
+                <p class="item__link">東京本社について</p>
+              </section>
+            </a>
+          </li>
+          <li class="item">
+            <a href="/about/iwate/">
+              <section class="item__inner">
+                <figure class="item__img"><img src="/assets/img/about/about_img02.jpg" alt="クーシーラボ 岩手"></figure>
+                <h2 class="item__title"><span>Coosy Lab. Iwate</span><small>クーシーラボ 岩手</small></h2>
+                    <p class="item__text">岩手の滝沢市、岩手県立大学の中にあるクーシーラボ岩手です。マークアップエンジニア、<br class="styleSp">プログラマが在籍しています。</p>
+                <p class="item__link">クーシーラボ 岩手について</p>
+              </section>
+            </a>
+          </li>
+          <li class="item">
+            <a href="/about/motors/">
+              <section class="item__inner">
+                <figure class="item__img"><img src="/assets/img/about/about_img03.jpg" alt="クーシーモータース"></figure>
+                <h2 class="item__title"><span>Coosy Motors</span><small>クーシーモータース</small></h2>
+                <p class="item__text">東京のあきる野市にあるクーシーモータースです。ガレージでは車のエンジンのカスタム<br class="styleSp">などしています。</p>
+                <p class="item__link">クーシーモータースについて</p>
+              </section>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </main><!-- #main -->
+  <?php
+    $footer_params = array(
+      array('name' => 'クーシーについて', 'url' => ''),
+    );
+  ?>
+  <?php include("../include/footer.php"); ?>
+</body>
+</html>
---
+++ offshore-master/recruit/faq/index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<title>採用に関する質問｜東京のWeb制作会社 クーシー（COOSY）</title>
+<meta name="description" content="クーシーの採用に関するよくある質問・FAQを掲載しています。採用面接などでいただく質問をQ&A形式でまとめました。一緒にクーシーをつくっていってくれる人との出会いを待っています。" />
+<?php include("../../include/head_favicon.php"); ?>
+<?php include("../../include/head_css.php"); ?>
+<!-- ページ個別CSS -->
+<link rel="stylesheet" href="/assets/css/recruit/faq/index.css">
+<!-- /ページ個別CSS -->
+<?php include("../../include/head_js.php"); ?>
+</head>
+<body>
+  <?php include("../../include/header.php"); ?>
+  <main class="main">
+    <div class="main__inner">
+      <div class="main__keyVisual">
+        <h1 class="main__keyVisual__title js-scrollMotion"><span class="title__shoulder">よくある質問</span><span class="title__large">Faq</span></h1>
+      </div>
+      <ul class="main__list js-scrollMotion">
+        <li class="main__list__item">
+          <section class="inner">
+            <h2 class="title jq-nextToggleSlide"><span class="title__inner">未経験者の採用はありますか？<span class="title__icon"></span></span></h2>
+            <div class="contents">
+              <p class="contents__answer">あります。</p>
+              <p class="contents__text">ものづくりに対する積極的な意欲があれば実務経験は不問です。<br>クーシーでは選考の中で、作品を最も重視しています。<br>実務経験のない方は学生時代の作品やデッサン、または自主制作された作品も選考対象です。</p>
+            </div>
+          </section>
+        </li>
+        <li class="main__list__item">
+          <section class="inner">
+            <h2 class="title jq-nextToggleSlide"><span class="title__inner">エントリーしてから採用までどのくらいの期間が必要ですか？<span class="title__icon"></span></span></h2>
+            <div class="contents">
+              <p class="contents__answer">約1ヶ月です。</p>
+              <p class="contents__text">面接の日程調整など個人差はありますが、1ヵ月程度みてください。<br>また、内定後の入社時期は1ヶ月以内とさせていただきます。</p>
+            </div>
+          </section>
+        </li>
+        <li class="main__list__item">
+          <section class="inner">
+            <h2 class="title jq-nextToggleSlide"><span class="title__inner">面接の時間帯は何時ごろ行われますか？<span class="title__icon"></span></span></h2>
+            <div class="contents">
+              <p class="contents__answer">平日20時開始まで可能です。</p>
+              <p class="contents__text">書類選考通過者には面接日時をお知らせいたします。<br>現職のある方は個別にご相談させていただいています。</p>
+            </div>
+          </section>
+        </li>
+        <li class="main__list__item">
+          <section class="inner">
+            <h2 class="title jq-nextToggleSlide"><span class="title__inner">平均年齢と男女比を教えてください。<span class="title__icon"></span></span></h2>
+            <div class="contents">
+              <p class="contents__answer">32歳です。</p>
+              <p class="contents__text">2019年4月時点で社員数38名。男性6割、女性4割です。<br>年齢や性別で業務内容や待遇は変わりません。若くても女性でも実力で評価しています。</p>
+            </div>
+          </section>
+        </li>
+        <li class="main__list__item">
+          <section class="inner">
+            <h2 class="title jq-nextToggleSlide"><span class="title__inner">社内の雰囲気を教えてください。<span class="title__icon"></span></span></h2>
+            <div class="contents">
+              <p class="contents__answer">仲が良いです。</p>
+              <p class="contents__text">20代?30代の社員が多く、社員同士の仲が良いのが特徴です。みんなでランチに行ったり、休日にWeb系のイベントに社員同士で参加することもあります。また、社員同士のコミュニケーションを深める場として、月に１回は就業時間中にフリースペースで懇親会を開催しています。</p>
+            </div>
+          </section>
+        </li>
+        <li class="main__list__item">
+          <section class="inner">
+            <h2 class="title jq-nextToggleSlide"><span class="title__inner">服装はスーツですか？<span class="title__icon"></span></span></h2>
+            <div class="contents">
+              <p class="contents__answer">自由です。</p>
+              <p class="contents__text">自分が最も働きやすい服装を選んでください。<br>ただしお客様に会うときやプレゼンテーションのときなどは、自己判断でTPOに配慮してくだされば結構です。</p>
+            </div>
+          </section>
+        </li>
+        <li class="main__list__item">
+          <section class="inner">
+            <h2 class="title jq-nextToggleSlide"><span class="title__inner">どのような環境で働いていますか？<span class="title__icon"></span></span></h2>
+            <div class="contents">
+              <p class="contents__answer">自分たちでデザインしたオフィスで働いています。</p>
+              <p class="contents__text">一人ひとりのデスクはオーダーメイドで、制作に集中できるゆったりとしたスペースを確保しています。その場で簡単な打ち合わせもできるレイアウトです。エントランスから見える大きなフリースペースは、廃校からいただいたデスクと椅子で作りました。社内ミーティングや、ランチタイム、休憩、懇親会など、仕事以外でもスタッフ同士のコミュニケーションの場になっています。殺風景だった壁一面のキャビネットには、チョークでクーシーロゴを描きました。もちろんオフィス内は禁煙です。給湯室には無料フルーツやジュースがあります。</p>
+            </div>
+          </section>
+        </li>
+        <li class="main__list__item">
+          <section class="inner">
+            <h2 class="title jq-nextToggleSlide"><span class="title__inner">履歴書等の個人情報はどのように取り扱い管理しいますか？<span class="title__icon"></span></span></h2>
+            <div class="contents">
+              <p class="contents__answer">採用選考以外の目的に使用することはありません。</p>
+              <p class="contents__text">なお、お預かりした履歴書・個人情報は利用後すみやかに処分しており、原則としてご返却できかねますのでご了承ください。<br>ポートフォリオのブックやその他作品は選考後ご要望に応じてお返しいたします。</p>
+            </div>
+          </section>
+        </li>
+        <li class="main__list__item">
+          <section class="inner">
+            <h2 class="title jq-nextToggleSlide"><span class="title__inner">残業時間はどのくらいありますか？<span class="title__icon"></span></span></h2>
+            <div class="contents">
+              <p class="contents__answer">2018年度の平均残業時間は20時間です。</p>
+              <p class="contents__text">クーシーは成果主義です。残業時間が長いことを評価しません。効率的に業務を進め時間を有効に使える方、さらに自分以外のメンバーの効率化も図れる方を高く評価します。</p>
+            </div>
+          </section>
+        </li>
+        <li class="main__list__item">
+          <section class="inner">
+            <h2 class="title jq-nextToggleSlide"><span class="title__inner">リモートワークは可能ですか？<span class="title__icon"></span></span></h2>
+            <div class="contents">
+              <p class="contents__answer">条件付きで可能です。</p>
+              <p class="contents__text">育児や介護など出社が困難と判断でき、且つお持ちのスキルによりリモートワークが可能です（実績あり）。クーシーでは、会社と社員が「良いカンケー」を築くことで、様々な働き方に対応していきます。</p>
+            </div>
+          </section>
+        </li>
+        <li class="main__list__item">
+          <section class="inner">
+            <h2 class="title jq-nextToggleSlide"><span class="title__inner">勤務時間は何時ですか？<span class="title__icon"></span></span></h2>
+            <div class="contents">
+              <p class="contents__answer">9:45?18:45でフレックスの部署もあります。</p>
+              <p class="contents__text">休憩は13:00?14:00です。デザイン部とクーシーラボ岩手は、フレックスを導入しています（コアタイム10:00?13:00）。</p>
+            </div>
+          </section>
+        </li>
+        <li class="main__list__item">
+          <section class="inner">
+            <h2 class="title jq-nextToggleSlide"><span class="title__inner">新卒ですが研修はありますか？<span class="title__icon"></span></span></h2>
+            <div class="contents">
+              <p class="contents__answer">あります。</p>
+              <p class="contents__text">新社会人研修、デザイン研修、コーディング研修などがあります。研修後、メンターとともにOJTで業務に慣れていきます。1?3年で一人前のプレイヤーとして活躍しています。</p>
+            </div>
+          </section>
+        </li>
+        <li class="main__list__item">
+          <section class="inner">
+            <h2 class="title jq-nextToggleSlide"><span class="title__inner">子どもがいる方はいますか？<span class="title__icon"></span></span></h2>
+            <div class="contents">
+              <p class="contents__answer">在籍しています。</p>
+              <p class="contents__text">2018年度は21%在籍しています。育児休暇中の方もいますし、業務後子どもを迎えに行く方もいます。代表も二児の父です。</p>
+            </div>
+          </section>
+        </li>
+      </ul>
+      <p class="main__backLink js-scrollMotion"><a href="/recruit/">リクルートTOP</a></p>
+    </div>
+  </main>
+  <?php
+    $footer_params = array(
+      array('name' => 'リクルート', 'url' => '/recruit/'),
+      array('name' => 'よくある質問', 'url' => ''),
+    );
+  ?>
+  <?php include("../../include/footer.php"); ?>
+</body>
+</html>
